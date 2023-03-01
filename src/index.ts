2c5b1d3e137290db27aefe2908eb1ac589c5e4e3
Hikaru Yoshino <osushicrusher@gmail.com>
2023-03-01 23:26:38 +0900

    fix: Get a Bearer token from reqHeader

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ const API_END_POINT = "https://api.notion.com/v1";
 
 export default {
   async fetch(request: Request) {
-    const NOTION_API_KEY = request.headers.get("Authorization");
+    const authHeader = request.headers.get("Authorization");
+    if (!authHeader) {
+      return new Response("Missing Authorization header", { status: 401 });
+    }
+    // split the auth header to get the Bearer token
+    const authToken = authHeader.split(" ")[1];
     const url = new URL(request.url);
     const requestUrl = `${API_END_POINT}${url.pathname}`;
     const result = await fetch(requestUrl, {
       body: request.body,
       headers: {
-        Authorization: "Bearer " + NOTION_API_KEY,
+        Authorization: "Bearer " + authToken,
         "Notion-Version": "2022-06-28",
       },
       method: request.method,

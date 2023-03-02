3ab55738db81e2a2a302b1a09f76c72013b6ada6
Hikaru Yoshino <osushicrusher@gmail.com>
2023-03-02 22:50:26 +0900

    feat: check if the NOTION_API_KEY matches.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+type Env = {
+  NOTION_API_KEY: string;
+};
+
 const API_END_POINT = "https://api.notion.com/v1";
 
 export default {
-  async fetch(request: Request) {
+  async fetch(request: Request, env: Env) {
     const authHeader = request.headers.get("Authorization");
+
     if (!authHeader) {
       return new Response("Missing Authorization header", { status: 401 });
     }
     // split the auth header to get the Bearer token
     const authToken = authHeader.split(" ")[1];
+
+    if (authToken !== env.NOTION_API_KEY) {
+      return new Response("NOTION_API_KEY does not match", { status: 401 });
+    }
+
     const url = new URL(request.url);
     const requestUrl = `${API_END_POINT}${url.pathname}`;
     const result = await fetch(requestUrl, {
